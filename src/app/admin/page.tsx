4617177d4d6658285cf46c69e443bb530c11b0dc
Saarth <saarth.pandya@gmail.com>
2025-07-10 20:40:54 -0400

    favicon update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,7 @@ export default function AdminPage() {
   return (
     <div>
       <div className="mb-6">
-        <h1 className="text-3xl font-bold tracking-tight">Administrator Dashbard</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Administrator Dashboard</h1>
         <p className="text-muted-foreground">Create new sessions and manage existing ones.</p>
       </div>
       <AdminView />
